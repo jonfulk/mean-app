afe4f603470114d0ff0b4c765c937e64e358edb3
Jonathan Fulk <jonfulk@gmail.com>
2017-06-23 10:26:31 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,47 @@
+const path = require('path');,
+      express = require('express'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      passport = require('passport'),
+      mongoose = require('mongoose'),
+      config = require('./config/database');
+
+// Connect To Database
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: ' + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Port Number
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+
+// Routes
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+  res.send('Index');
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
+});
