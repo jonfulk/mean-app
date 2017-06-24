260dbb3b258277c4cfd1759d50fe6c871f947088
Jonathan Fulk <jonfulk@gmail.com>
2017-06-24 07:37:35 -0500

    Create registration form and validate service.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -9,7 +12,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-import { RouterModule, Routes } from '@angular/router';
+import { ValidateService } from './services/validate.service';
 
 const appRoutes: Routes = [
   {path:'', component: HomeComponent},
@@ -31,9 +34,11 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
