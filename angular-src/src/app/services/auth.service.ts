f31bfde3ab82175f7c1544bfa1fae36d8186f17f
Jonathan Fulk <jonfulk@gmail.com>
2017-06-25 08:15:32 -0500

    Protect routes using AuthGuard service.

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { tokenNotExpired } from 'angular2-jwt'
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -21,6 +22,23 @@ export class AuthService {
         headers.append('Content-Type', 'application/json');
         return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
           .map(res => res.json());
+    }
+
+    getProfile() {
+        let headers = new Headers();
+        this.loadToken();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Authorization', this.authToken);
+        return this.http.get('http://localhost:3000/users/profile', {headers: headers})
+          .map(res => res.json());
+    }
+
+    loadToken() {
+      this.authToken = localStorage.getItem('id_token');
+    }
+
+    loggedIn() {
+      return tokenNotExpired('id_token');
     }
 
     storeUserData(token, user) {
